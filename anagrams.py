6a5255305e3aef872cb441c159b80761e798cc35
kbousquet <kbousquet95@gmail.com>
2024-03-21 22:08:30 -0500

    Update anagrams.py

--- anagrams.py
+++ anagrams.py
@@ -1,13 +1,17 @@
-# This script takes in two inputs and determines if they are anagrams of each other.
+# This script takes in two string inputs and determines if they are anagrams of each other.
+# Anagrams are when one word or phrase, rearranged, creates a different word or phrase.
+# Example: "iceman" and "cinema"
 
-input_a = input("Input A ")
-input_b = input("Input B ")
+def check_if_anagram(input_a, input_b):
+	sorted_a = sorted(input_a.lower())
+	sorted_b = sorted(input_b.lower())
 
-sorted_a = sorted(input_a.lower())
-sorted_b = sorted(input_b.lower())
+	if sorted_a == sorted_b:
+		print(input_a + " and " + input_b + " are anagrams.")
+	else:
+		print(input_a + " and " + input_b + " are not anagrams.")
 
-if sorted_a == sorted_b:
-	print(input_a + " and " + input_b + " are anagrams.")
 
-else:
-	print(input_a + " and " + input_b + " are not anagrams.")
+check_if_anagram("iceman", "cinema")
+
+check_if_anagram("dog", "cat")
