46ab94e1b1eefab265c8dfb3461f10b4517bc821
kbousquet <kbousquet95@gmail.com>
2024-03-21 22:24:24 -0500

    Update birthday.py

--- birthday.py
+++ birthday.py
@@ -1,31 +1,36 @@
-# This script prompts for the user's birthday, then returns "happy birthday" with the correct number ending.
+# This script prompts for the user's birthday, then returns "happy birthday" with the correct suffix.
 
-age = input("What is your age? ")
+def print_happy_birthday(age):
+    if age.startswith("1"):
+        # for 1
+        if age == "1":
+            suffix = "st"
+        # for 11-19
+        else:
+            suffix = "th"
 
-if age.startswith("1"):
-    # for 1
-    if age == "1":
-        message = "Happy " + str(age) + "st Birthday!"
-        print(message)
-    # for 11-19
+    # all other numbers, outside of 1 and 11, that ends with 1
+    elif age.endswith("1"):
+        suffix = "st"
+
+    elif age.endswith("2"):
+        suffix = "nd"
+
+    elif age.endswith("3"):
+        suffix = "rd"
+
+    # All other numbers, outside of 14-19, that end with 4-9
     else:
-        message = "Happy " + str(age) + "th Birthday!"
-        print(message)
+        suffix = "th"
 
-# all other numbers, outside of 1 and 11, that ends with 1
-elif age.endswith("1"):
-    message = "Happy " + str(age) + "st Birthday!"
-    print(message)
+    print("Happy " + str(age) + str(suffix) + " Birthday!")
 
-elif age.endswith("2"):
-    message = "Happy " + str(age) + "nd Birthday!"
-    print(message)
+print_happy_birthday("27")
+print_happy_birthday("32")
+print_happy_birthday("73")
 
-elif age.endswith("3"):
-    message = "Happy " + str(age) + "rd Birthday!"
-    print(message)
 
-# All other numbers, outside of 14-19, that end with 4-9
-else:
-    message = "Happy " + str(age) + "th Birthday!"
-    print(message)
+# Output:
+# Happy 27th Birthday!
+# Happy 32nd Birthday!
+# Happy 73rd Birthday!
