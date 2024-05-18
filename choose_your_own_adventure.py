d39802f0c47af87e45d0fae7551fe8c99426409f
Kathryn Bousquet <kbousquet95@gmail.com>
2024-05-17 20:19:53 -0500

    Update choose_your_own_adventure.py

--- choose_your_own_adventure.py
+++ choose_your_own_adventure.py
@@ -28,7 +28,31 @@ if answer == "left":
 
 elif answer == "right":
 	print('You decide to take the right path.\n')
+	answer = input('You come to a bridge that does not look safe. Do you try to "cross" the bridge or turn "back?"\n')
+	if answer == "back":
+		print('You turn to head back, only to find yourself face to face with a group of bandits that had been')
+		print('tailing you. You are outnumbered and are swiftly taken down.')
+		print("You have died.\n")
+	elif answer == "cross":
+		print("You manage to cross the bridge just in time before the structure collapses. There is no turning back now.\n")
+		print('You continue your journey and eventually come across a stranger in your path.')
+		answer = input('Do you "talk" to them or "ignore" them?\n')
+		if answer == "talk":
+			print('The stranger is so happy that you stopped to talk to them that they give you all of their gold!')
+			print("You have won!\n")
+		elif answer == "ignore":
+			print("You give the stranger a wide berth and only realize too late of your offense until their dagger is in your back")
+			print("You have died.\n")
+	else:
+		print("You both hesitate to move and find yourselves in a staring contest that last days on end until you both")
+		print("succumb to hunger and thirst.")
+		print("You have died.\n")
+
+
 else:
 	print('Invalid response. You decide to attempt casting a detection spell to determine which is the correct path, but')
 	print('your lack of magical prowess caused the spell to backfire at you.')
 	print("You have died.\n")
+
+
+print(f"You gave it your best, {name}")
